440e7810514ccf93f8464700641e4892b16e08f7
Moayad-AK <moayadalakkad979@gmail.com>
2025-09-10 15:40:31 +0300

    update

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -3,6 +3,6 @@
         Store Aboutpage
     </x-slot:title>
     <div class="container px-4 px-lg-5 my-5">
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facilis temporibus inventore incidunt fuga exercitationem ea nisi, quia nihil mollitia molestiae tenetur eos delectus minima neque iusto soluta deserunt quae quisquam!</p>
+        <p>this is moayad project</p>
     </div>
 </x-layout>
